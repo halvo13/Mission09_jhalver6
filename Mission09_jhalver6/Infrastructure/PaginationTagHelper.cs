7496e22fbf90af10187b6e9ede5b82effef97ea6
jhalver6 <jhalver6@byu.edu>
2023-03-01 22:42:07 -0700

    Add project files.

---
+++ Mission09_jhalver6/Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Mission09_jhalver6.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission09_jhalver6.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PaginationTagHelper : TagHelper
+    {
+        //Dynamically create page links
+        private IUrlHelperFactory uhf;
+
+        public PaginationTagHelper(IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+        public PageInfo PageModel { get; set; }
+        public string PageAction { get; set; }
+       
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i <= PageModel.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            output.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
